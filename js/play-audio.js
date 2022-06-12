481ae81b6bcc64c4e3086f39bda955f185856e21
harpia01 <andre23arruda@gmail.com>
2022-06-12 11:51:54 -0400

    refactor: Add functions

--- js/play-audio.js
+++ js/play-audio.js
@@ -13,29 +13,37 @@ const keyboard = [
     { key: 'c', id: 'key_tom'},
 ]
 
+function playSound(button) {
+    const button_id = button.id
+    const sound = document.querySelector(`#audio_${ button_id }`)
+    if (currentSong !== button_id) {
+        sound.play()
+    } else {
+        sound.pause()
+        sound.currentTime = 0
+        sound.play()
+    }
+    currentSong = button_id
+}
+
+function keyboardClick(key) {
+    const button_id = key.id
+    const button = document.querySelector(`#${ button_id }`)
+    button.click()
+    button.classList.add('active')
+    setTimeout(() => button.classList.remove('active'), 200)
+}
+
 buttons.forEach(button => (
     button.addEventListener('click', () => {
-        const button_id = button.id
-        const sound = document.querySelector(`#audio_${ button_id }`)
-        if (currentSong !== button_id) {
-            sound.play()
-        } else {
-            sound.pause()
-            sound.currentTime = 0
-            sound.play()
-        }
-        currentSong = button_id
+        playSound(button)
     })
 ))
 
 document.addEventListener('keydown', e => {
     keyboard.forEach(key => {
         if (e.key === key.key) {
-            const button_id = key.id
-            const button = document.querySelector(`#${ button_id }`)
-            button.click()
-            button.classList.add('active')
-            setTimeout(() => button.classList.remove('active'), 200)
+            keyboardClick(key)
         }
     })
 })
