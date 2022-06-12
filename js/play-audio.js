8b68ac0ae7d1f76c5b8a88f3701d2c1a91282b0d
harpia01 <andre23arruda@gmail.com>
2022-06-11 20:02:10 -0400

    done

---
+++ js/play-audio.js
@@ -0,0 +1,41 @@
+const buttons = document.querySelectorAll('.key')
+var currentSong = null
+
+const keyboard = [
+    { key: 'q', id: 'key_pom'},
+    { key: 'w', id: 'key_clap'},
+    { key: 'e', id: 'key_tim'},
+    { key: 'a', id: 'key_puff'},
+    { key: 's', id: 'key_splash'},
+    { key: 'd', id: 'key_toim'},
+    { key: 'z', id: 'key_psh'},
+    { key: 'x', id: 'key_tic'},
+    { key: 'c', id: 'key_tom'},
+]
+
+buttons.forEach(button => (
+    button.addEventListener('click', () => {
+        const button_id = button.id
+        const sound = document.querySelector(`#som_${ button_id }`)
+        if (currentSong !== button_id) {
+            sound.play()
+        } else {
+            sound.pause()
+            sound.currentTime = 0
+            sound.play()
+        }
+        currentSong = button_id
+    })
+))
+
+document.addEventListener('keydown', e => {
+    keyboard.forEach(key => {
+        if (e.key === key.key) {
+            const button_id = key.id
+            const button = document.querySelector(`#${ button_id }`)
+            button.click()
+            button.classList.add('active')
+            setTimeout(() => button.classList.remove('active'), 200)
+        }
+    })
+})
